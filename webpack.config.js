b1dfb475b1e76d636f37ac4e529c6ea4838b85a9
Max Lebedev <truesilv@gmail.com>
2017-01-23 20:25:29 +0500

    webpack migrate

--- webpack.config.js
+++ webpack.config.js
@@ -11,25 +11,42 @@ module.exports = {
         filename: 'test.js'
     },
     module: {
-        loaders: [{
+        rules: [{
             test: /\.ts$/,
             exclude: /node_modules/,
-            loader: 'babel-loader?presets[]=es2016&presets[]=es2015!ts-loader'
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                    presets: ['es2015', 'es2016']
+                }
+            }, 'ts-loader']            
         }, {
             test: '\.js$',
             exclude: /node_modules/,
-            loader: 'babel',
-            query: {
-                presets: ['es2016', 'es2015']
+            use: {
+                loader: 'babel-loader',
+                options: {
+                   presets: ['es2016', 'es2015'] 
+                }
             }
         }, {
             test: /\.css$/,
             exclude: /node_modules/,
-            loaders: ['style-loader', 'css-loader?importLoaders=1', 'postcss-loader']
+            use: {
+                loaders: [
+                    'style-loader', 
+                    { 
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1
+                        }
+                    },
+                    'postcss-loader']
+            }
         }]
     },
     resolve: {
-        extensions: ['', '.ts', '.js']
+        extensions: ['.ts', '.js']
     },
     devtool: 'source-map'
 };
